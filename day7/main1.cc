576779437964cadb6d99619af521f648b50c5be6
Nathaniel Bielanski <nbielans@purdue.edu>
2023-12-21 22:33:35 -0500

    day7 problem1

---
+++ day7/main1.cc
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <unordered_map>
+
+#define INPUT_FILE "input.txt"
+
+enum CARDS { _2=2, _3, _4, _5, _6, _7, _8, _9, _T, _J, _Q, _K, _A };
+enum HAND_TYPE  { HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, FULL_HOUSE, FOUR_OF_A_KIND, FIVE_OF_A_KIND };
+
+class Hand {
+    private:
+        std::vector<CARDS> hand;
+        HAND_TYPE type;
+        int bid;
+    public:
+        Hand(const std::vector<CARDS>& _hand, int _bid) {
+            hand = _hand;
+            bid = _bid;
+            std::unordered_map<CARDS, int> occurances;
+            for(const CARDS& card: hand) {
+                try {
+                    occurances.at(card)++;
+                } catch(std::out_of_range) {
+                    occurances[card] = 1;
+                }
+            }
+
+            if(occurances.size()==5 || occurances.size()==4 || occurances.size()==1) {
+                type = (occurances.size()==5) ? HIGH_CARD : 
+                       (occurances.size()==4) ? ONE_PAIR  :
+                                                FIVE_OF_A_KIND;
+            } else if(occurances.size()==3) {
+                type = THREE_OF_A_KIND;
+                for(const auto& occ: occurances) {
+                    if(occ.second==2) {
+                        type = TWO_PAIR;
+                        break;
+                    }
+                }
+            } else { // occurances.size()==2
+                type = FULL_HOUSE;
+                for(const auto& occ: occurances) {
+                    if(occ.second==4) {
+                        type = FOUR_OF_A_KIND;
+                        break;
+                    }
+                }
+            }
+        }
+
+        int getBid() const { return bid; }
+        HAND_TYPE getType() const { return type; }
+        std::vector<CARDS>::const_iterator cbegin() const { return hand.cbegin(); }
+
+        bool operator<(const Hand& other) {
+            if(type != other.getType()) {
+                return type < other.getType();
+            }
+
+            auto it = hand.cbegin();
+            auto oit = other.cbegin();
+            while(it!=hand.end()) {
+                if((*it)!=(*oit)) {
+                    return (*it)<(*oit);
+                }
+                it++;
+                oit++;
+            }
+
+            return false;
+        }
+};
+
+bool isNum(char c) {
+    return (c >= '0') && (c <='9');
+}
+
+int getNum(const std::string& line, int i) {
+    int val = 0;
+    
+    while(isNum(line[i])) {
+        val *= 10;
+        val += line[i]-'0';
+        i++;
+    }
+
+    return val;
+}
+
+CARDS getCard(char c) {
+    if(c<='9') {
+        return (CARDS) (c-'0');
+    }
+
+    switch(c) {
+        case 'A': return _A;
+        case 'K': return _K;
+        case 'Q': return _Q;
+        case 'J': return _J;
+        case 'T': return _T; 
+    }
+}
+
+void setPlayers(std::ifstream& file, std::list<Hand>& players) {
+    std::string line;
+    std::vector<CARDS> hand(5);
+    int bid;
+
+    while(!file.eof()) {
+        std::getline(file, line);
+        for(int i=0; i<5; i++) {
+            hand[i] = getCard(line[i]);
+        }
+        bid = getNum(line, 6);
+        
+        players.push_back(Hand(hand, bid));
+    }    
+}
+
+long getWinnings(const std::list<Hand>& players) {
+    long winnings = 0;
+    int i = 1;
+    for(auto player: players) {
+        std::cout << player.getBid() << ' ' << player.getType() << std::endl;
+        winnings += i*player.getBid();
+        i++;
+    }
+
+    return winnings;
+}
+
+int main(void) {
+    std::list<Hand> players;
+    std::ifstream file;
+    file.open(INPUT_FILE);
+    if(!file.is_open()) {
+        return 1;
+    }
+
+    setPlayers(file, players);
+
+    players.sort();
+
+    long winnings = getWinnings(players);
+
+    std::cout << "Winnings: " << winnings << std::endl;    
+    return 0;
+}
